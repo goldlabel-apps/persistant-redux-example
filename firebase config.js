0a5afdc36041821dae72602404aed0d29ae309bb
listingslab <listingslab@gmail.com>
2019-06-07 14:05:32 +1000

    Delete error

--- firebase config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC0ObHwLECJNObdpza00uFSF7XoJEdqQZo",
-    authDomain: "git-fundamentals.firebaseapp.com",
-    databaseURL: "https://git-fundamentals.firebaseio.com",
-    projectId: "git-fundamentals",
-    storageBucket: "git-fundamentals.appspot.com",
-    messagingSenderId: "913483269054",
-    appId: "1:913483269054:web:d8b262149fd673e3"
-  };
