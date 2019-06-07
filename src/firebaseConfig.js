55901d3410d113464f647260c4d19e0606c24865
listingslab <listingslab@gmail.com>
2019-06-07 15:48:24 +1000

    feat(react): turn off unsused

--- src/firebaseConfig.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGESENDERID
-};
