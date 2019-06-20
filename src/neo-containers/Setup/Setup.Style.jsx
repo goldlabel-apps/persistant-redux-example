61e6a187831e5f72163986ca7f6e00d39061c234
listingslab <listingslab@gmail.com>
2019-06-20 22:51:30 +1000

    feat(Setup): user

--- src/neo-containers/Setup/Setup.Style.jsx
+++ src/neo-containers/Setup/Setup.Style.jsx
@@ -7,4 +7,14 @@ export const styles = theme => ({
         // border: '1px solid green',
         padding: 10,
     },
+
+    orgCard:{
+        // background: '#def2f6',
+        padding: theme.spacing(1),
+        borderRadius: 'none',
+        border: 'none',
+        // boxShadow: 'none',
+    },
+
+
 });
