58bfbbffe2393f385714ad512ecf4fbeba9adbd3
weizang-75 <github@wei-zang.com>
2019-06-23 11:16:14 +1000

    fix(0.1.8)

--- src/neo-containers/AppShell/AppShell.Style.jsx
+++ src/neo-containers/AppShell/AppShell.Style.jsx
@@ -3,4 +3,8 @@ import commonStyles from "../../theme/commonStyles";
 
 export const styles = theme => ({
     ...commonStyles(theme),
+
+    docsify: {
+        border: '1px solid red',
+    },
 });
